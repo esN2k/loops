9996fe8392995b982b1422b1629c57e238a5a925
BuildTools <dodikbalaman@hotmail.com>
2021-11-07 21:44:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i = i+ 2) {
+            System.out.println(i);
+        }
+        System.out.println("The loop is halfway over");
+
+        for (int i = 6; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("The loop is over");
+    }
+}
